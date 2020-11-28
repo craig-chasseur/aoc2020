e76dd4b4b5a178005c52c2e0e251adca69139601
Craig Chasseur <spoonboy42@gmail.com>
2020-11-27 21:28:24 -0800

    Some basic file I/O routines

---
+++ io/io.cc
@@ -0,0 +1,57 @@
+#include "io/io.h"
+
+#include <fstream>
+#include <ios>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_split.h"
+
+namespace aoc2020 {
+
+absl::StatusOr<std::string> ReadFile(const char* filename) {
+  std::ifstream stream(filename);
+  if (!stream) {
+    return absl::Status(absl::StatusCode::kNotFound,
+                        absl::StrCat("Unable to open file: ", filename));
+  }
+
+  std::string buffer;
+  stream.seekg(0, std::ios::end);
+  buffer.resize(stream.tellg());
+  stream.seekg(0, std::ios::beg);
+  stream.read(&buffer[0], buffer.size());
+  stream.close();
+
+  return buffer;
+}
+
+absl::StatusOr<std::vector<std::string>> ReadLinesFromFile(
+    const char* filename) {
+  std::ifstream stream(filename);
+  if (!stream) {
+    return absl::Status(absl::StatusCode::kNotFound,
+                        absl::StrCat("Unable to open file: ", filename));
+  }
+
+  std::vector<std::string> all_lines;
+  std::string line;
+  while (std::getline(stream, line)) {
+    all_lines.emplace_back(std::move(line));
+  }
+  stream.close();
+  return all_lines;
+}
+
+absl::StatusOr<std::vector<std::string>> ReadCommaDelimitedFile(
+    const char* file) {
+  absl::StatusOr<std::string> maybe_contents = ReadFile(file);
+  if (!maybe_contents.ok()) return maybe_contents.status();
+  return absl::StrSplit(*maybe_contents, ',');
+}
+
+}  // namespace aoc2020
---
+++ io/io.h
@@ -0,0 +1,27 @@
+#ifndef IO_IO_H_
+#define IO_IO_H_
+
+#include <string>
+#include <vector>
+
+#include "absl/status/statusor.h"
+
+namespace aoc2020 {
+
+// Returns the whole contents of the text file specified by `filename`, or an
+// error if the file could not be read.
+absl::StatusOr<std::string> ReadFile(const char* filename);
+
+// Returns each of the lines in the text file specified by `filename`, or an
+// error if the file could not be read.
+absl::StatusOr<std::vector<std::string>> ReadLinesFromFile(
+    const char* filename);
+
+// Returns comma-delimited strings from `filename`, or an error if the file
+// could not be read.
+absl::StatusOr<std::vector<std::string>> ReadCommaDelimitedFile(
+    const char* file);
+
+}  // namespace aoc2020
+
+#endif  // IO_IO_H_
