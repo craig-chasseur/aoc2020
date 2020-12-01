c86816272008fd844fadaa760a61c1a2e2710df3
Craig Chasseur <spoonboy42@gmail.com>
2020-11-30 23:46:25 -0800

    Adopt CHECK macros

--- puzzles/day_01_part_01/main.cc
+++ puzzles/day_01_part_01/main.cc
@@ -1,11 +1,10 @@
-#include <cassert>
-#include <cstddef>
 #include <iostream>
 #include <string>
 #include <vector>
 
 #include "absl/status/statusor.h"
 #include "absl/strings/numbers.h"
+#include "util/check.h"
 #include "util/io.h"
 
 namespace {
@@ -13,12 +12,11 @@ namespace {
 std::vector<int> GetItems(const char* filename) {
   absl::StatusOr<std::vector<std::string>> lines =
       aoc2020::ReadLinesFromFile(filename);
-  assert(lines.ok());
+  CHECK_OK(lines);
   std::vector<int> items;
   for (const std::string& line : *lines) {
     int parsed;
-    const bool ok = absl::SimpleAtoi(line, &parsed);
-    assert(ok);
+    CHECK(absl::SimpleAtoi(line, &parsed));
     items.push_back(parsed);
   }
   return items;
@@ -32,14 +30,13 @@ int Solve(const std::vector<int>& items) {
       }
     }
   }
-  assert(false);
-  return -1;
+  CHECK_FAIL();
 }
 
 }  // namespace
 
 int main(int argc, char** argv) {
-  assert(argc == 2);
+  CHECK(argc == 2);
   std::vector<int> items = GetItems(argv[1]);
   std::cout << Solve(items) << "\n";
   return 0;
